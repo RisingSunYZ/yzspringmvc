f291af546281bdbc5955adc18d95b80058ce5346
yangzhao <357823669@qq.com>
2019-12-25 19:25:00 +0800

    Initial commit

---
+++ src/main/java/com.yz.controller/MyController.java
@@ -0,0 +1,16 @@
+package com.yz.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Description:
+ * @Author:YangZhao
+ * @Since:2019/12/25 10:48
+ * @Version:1.1.0
+ * @Copyright:Copyright (c) 浙江蘑菇加电子商务有限公司 2018 ~ 2026 版权所有
+ */
+
+
+@Controller
+public class MyController {
+}
